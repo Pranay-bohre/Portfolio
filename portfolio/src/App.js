f3f1c5fad80a19b093941f30e3ae9b687b2a62ca
Pranay-Bohre <pranaybohreofficial@gmail.com>
2023-03-19 02:15:04 +0530

    committed to portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Header from './components/Header';
+// import PlaceToVisit from './components/PlaceToVisit';
 
 const useStyles = makeStyles((theme) => ({
   root: {
